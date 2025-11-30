1fb6b5e4a24eb1a3812313bc7aa03206fa7d2a7b
LORDENDC <143540107+LORDENDC@users.noreply.github.com>
2025-11-30 12:25:48 -0300

    Update upgrades.h

--- upgrades.h
+++ upgrades.h
@@ -4,14 +4,25 @@
 typedef struct {
     int nivel_colheita;
     int nivel_lavagem;
+    int nivel_corte;
     int nivel_extracao;
-    int nivel_envase;
-    int custo[4];
+    int nivel_embalagem;
+
+    int custo_colheita;
+    int custo_lavagem;
+    int custo_corte;
+    int custo_extracao;
+    int custo_embalagem;
+
 } Upgrades;
 
 extern Upgrades upgrades;
 
 void init_upgrades();
-int tentar_upgrade(int etapa);
+int tentar_upgrade_colheita();
+int tentar_upgrade_lavagem();
+int tentar_upgrade_corte();
+int tentar_upgrade_extracao();
+int tentar_upgrade_embalagem();
 
 #endif
